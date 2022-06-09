350f110ff7c7c5ca04abf8f63b62b620aae0b176
Md. Mahmudul Hasan <69659701+antor9732@users.noreply.github.com>
2022-06-09 14:03:20 +0600

    Add files via upload

---
+++ Online shop/public_html/js/main.js
@@ -0,0 +1,25 @@
+/**First Slider */
+
+$('.slider-one')
+.not(".slick-intialized")
+.slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+
+}); 
+
+/**second Slider */
+
+//$('.slider-two')
+//.not(".slick-intialized")
+//.slick({
+    //prevArrow:".site-slider-two .prev",
+   // nextArrow:".site-slider-two .next",
+   // slidesToShow:5,
+    //slideaToShow:1,
+    //autoplayspeed:3000
+
+//}); 
